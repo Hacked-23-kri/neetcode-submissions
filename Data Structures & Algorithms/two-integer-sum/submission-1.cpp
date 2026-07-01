5060d4ef7d83992b5074f2d449bc9b437a5a175f
Krishna Rajbhar <rajbhar.krishna2006@gmail.com>
2026-07-01 19:55:38 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+
+        for(int i=0;i<nums.size();i++){
+            int num = nums[i];
+            int rem = target - num;
+            if(mp.find(rem) != mp.end()){
+                return {mp[rem], i};
+            }
+            mp[num] = i;
+        }
+    }
+};
